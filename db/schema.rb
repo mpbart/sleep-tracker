eb0ea13d0fa2218d2c425e15ab89f43743acd141
mpbart <michael.barton0924@gmail.com>
2022-06-30 11:25:51 -0400

    Add username to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_134109) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_30_152238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_21_134109) do
     t.string "hashed_pin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
   end
 
   add_foreign_key "boolean_data_points", "sleep_qualities"
